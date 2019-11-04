93799d35d327603abd3a785532c1be0f132a4a0f
sam-malanchuk <malanchuksam@gmail.com>
2019-11-04 18:32:14 -0500

    created state with defualt values and the nextStep/prevStep components to go through the form

--- wowo-app/src/components/WasherSignUpForm.js
+++ wowo-app/src/components/WasherSignUpForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import { MDBProgress } from 'mdbreact';
 import WasherSignUpPersonal from './WasherSignUpPersonal.js';
@@ -6,18 +6,44 @@ import WasherSignUpPersonal from './WasherSignUpPersonal.js';
 // import WasherSignUpReview from './WasherSignUpReview.js';
 import '../App.css';
 
-export default function() {
-  return (
-    <MDBContainer>
-      <MDBRow center>
-        <MDBCol md="6">
-          <div className="form-group">
-            <p className="h5 text-center mb-4">Sign up</p>
-            <MDBProgress className="my-2" material value={33} color="info" />
-            <WasherSignUpPersonal />
-          </div>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  );
+export class WasherSignUpForm extends Component {
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  }
+
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  }
+
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  }
+
+  render() {
+    return (
+      <MDBContainer>
+        <MDBRow center>
+          <MDBCol md="6">
+            <div className="form-group">
+              <p className="h5 text-center mb-4">Sign up</p>
+              <MDBProgress className="my-2" material value={33} color="info" />
+              <WasherSignUpPersonal />
+            </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    );
+  }
 };
+
+export default WasherSignUpForm;
