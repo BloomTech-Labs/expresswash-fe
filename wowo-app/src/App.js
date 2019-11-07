5a6df41462668f1c94b746c9b071e42e5302a1ab
Ryan Ansberry <ansman92@gmail.com>
2019-11-06 17:25:44 -0800

    Styling adjustment v1

--- wowo-app/src/App.js
+++ wowo-app/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
 
-
-
 function App() {
   return (
     <div className="App">
       <h1>WoWo App</h1>
-
     </div>
   );
 }
--- wowo-app/src/components/NavigationPage.js
+++ wowo-app/src/components/NavigationPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import './NavigationPage.css';
+import { MDBBtn } from "mdbreact";
 
 export default function NavigationPage() {
     return (
@@ -11,8 +10,8 @@ export default function NavigationPage() {
                     <p className="nav-slogan">We're the new way to wash your car.</p>
                 </div>
                 <div className="header-buttons">
-                    <button className="sign-up-btn btn">Sign Up</button>
-                    <button className="login-btn btn">Log In</button>
+                    <MDBBtn className="sign-up-btn" color="primary">Sign Up</MDBBtn>
+                    <MDBBtn className="login-btn">Log In</MDBBtn>
                 </div>
             </section>
 
@@ -21,7 +20,7 @@ export default function NavigationPage() {
                     <h3 className="nav-info-header">We Bring The Wash To You</h3>
                     <p className="nav-info-content">WoWo is an all new approach to car washing. Instead of driving to the carwash and waiting in long lines, we bring the car wash to you. Just select your wash, type of car, and time and our washers will come to you to perform a quality car wash. Get an instant quote here:</p>
 
-                    <button className="quote-btn">Instant Quote</button>
+                    <MDBBtn className="quote-btn" color="primary">Instant Quote</MDBBtn>
                 </div>
 
                 <div className="info-card eco">
@@ -34,7 +33,7 @@ export default function NavigationPage() {
                     <p className="nav-info-content">WoWo welcomes anyone whoe enjoys washing, detailing, or even just looking to make some extra income. All prospective washers must go through training to learn practices and standards. Once the training is complete, the washer can then use the app to find users in need of a carwash. Once the job is completed and the customer satisfied, the washer receives a cut of the cost as well as tips.</p>
                 </div>
 
-                <button className="washer-sign-up-btn">Become a Washer</button>
+                <MDBBtn className="washer-sign-up-btn" color="primary">Become a Washer</MDBBtn>
             </section>
         </div>
     )
