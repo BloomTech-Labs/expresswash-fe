dce7e5bc854cb9b08ebe756041c1c0f1d671d0b8
Ryan Ansberry <ansman92@gmail.com>
2019-11-07 18:31:04 -0800

    Added styling to landing page layout.

--- wowo-app/src/App.js
+++ wowo-app/src/App.js
@@ -6,7 +6,7 @@ import LandingPage from './components/LandingPage.js';
 function App() {
   return (
     <div className="App">
-      <h1>WoWo App</h1>
+      {/* <h1>WoWo App</h1> */}
       <LandingPage />
     </div>
   );
--- wowo-app/src/components/LandingPage.js
+++ wowo-app/src/components/LandingPage.js
@@ -2,40 +2,43 @@ import React from 'react';
 import { MDBBtn, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 import './LandingPage.css';
-import logo from '../images/wowo-logo-full.JPG'
+import logo from '../images/wowo-logo-full.jpg';
 
 export default function LandingPage() {
     return (
-        <MDBContainer className="nav-container">
+        <MDBContainer className="landing-container">
             <section className="header-container">
-                <div className="nav-header">
-                    <h1 className="nav-main-header">WoWo</h1>
-                    <image src='./'/>
-                    <p className="nav-slogan">We're the new way to wash your car.</p>
-                </div>
-                <div className="header-buttons">
+                <MDBContainer className="landing-header">
+                    <MDBRow className="mb-4">
+                        <MDBCol>
+                            <img className="wowo-logo" src={logo} alt="logo-image"/>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+                <MDBContainer className="header-buttons-container">
                     <MDBBtn className="sign-up-btn" color="primary">Sign Up</MDBBtn>
+                    <p className="btn-or">or</p>
                     <MDBBtn className="login-btn">Log In</MDBBtn>
-                </div>
+                </MDBContainer>
             </section>
 
-            <section className="information">
-                <div className="info-card about">
-                    <h3 className="nav-info-header">We Bring The Wash To You</h3>
-                    <p className="nav-info-content">WoWo is an all new approach to car washing. Instead of driving to the carwash and waiting in long lines, we bring the car wash to you. Just select your wash, type of car, and time and our washers will come to you to perform a quality car wash. Get an instant quote here:</p>
+            <section className="landing-information">
+                <MDBContainer className="landing-info-card about">
+                    <h3 className="landing-info-header">We Bring The Wash To You</h3>
+                    <p className="landing-info-content">WoWo is an all new approach to car washing. Instead of driving to the carwash and waiting in long lines, we bring the car wash to you. Just select your wash, type of car, and time and our washers will come to you to perform a quality car wash. Get an instant quote here:</p>
 
-                    <MDBBtn className="quote-btn" color="primary">Instant Quote</MDBBtn>
-                </div>
+                    {/* <MDBBtn className="quote-btn" color="primary">Instant Quote</MDBBtn> */}
+                </MDBContainer>
 
-                <div className="info-card eco">
-                    <h3 className="nav-info-header">Eco-Friendly</h3>
-                    <p className="nav-info-content">WoWo is dedicated to delivering an eco-friendly carwash. All of our washers are trained to used eco-friendly cleaners and practices. That way we can deliver a spectacular car wash while protecting the enviroment.</p>
-                </div>
+                <MDBContainer className="landing-info-card">
+                    <h3 className="landing-info-header eco">Eco-Friendly</h3>
+                    <p className="landing-info-content">WoWo is dedicated to delivering an eco-friendly carwash. All of our washers are trained to used eco-friendly cleaners and practices. That way we can deliver a spectacular car wash while protecting the enviroment.</p>
+                </MDBContainer>
                 
-                <div className="info-card washers">
-                    <h3 className="nav-info-header">Earn Extra Income</h3>
-                    <p className="nav-info-content">WoWo welcomes anyone whoe enjoys washing, detailing, or even just looking to make some extra income. All prospective washers must go through training to learn practices and standards. Once the training is complete, the washer can then use the app to find users in need of a carwash. Once the job is completed and the customer satisfied, the washer receives a cut of the cost as well as tips.</p>
-                </div>
+                <MDBContainer className="landing-info-card washers">
+                    <h3 className="landing-info-header">Earn Extra Income</h3>
+                    <p className="landing-info-content">WoWo welcomes anyone whoe enjoys washing, detailing, or even just looking to make some extra income. All prospective washers must go through training to learn practices and standards. Once the training is complete, the washer can then use the app to find users in need of a carwash. Once the job is completed and the customer satisfied, the washer receives a cut of the cost as well as tips.</p>
+                </MDBContainer>
 
                 <MDBBtn className="washer-sign-up-btn" color="primary">Become a Washer</MDBBtn>
             </section>
