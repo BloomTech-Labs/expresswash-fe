b003ad87994f0c0ca2603bd647037e17c74a4d8c
Ryan Ansberry <ansman92@gmail.com>
2019-11-11 17:01:51 -0800

    Commit b4 pull

--- wowo-app/src/App.js
+++ wowo-app/src/App.js
@@ -6,7 +6,6 @@ import LandingPage from './components/LandingPage.js';
 function App() {
   return (
     <div className="App">
-      {/* <h1>WoWo App</h1> */}
       <LandingPage />
     </div>
   );
