a6021be36221337624ee3a1d05224149cd51fb94
sam-malanchuk <malanchuksam@gmail.com>
2019-11-14 21:56:28 -0500

    updated the washer form address populating to use component state

--- wowo-app/src/components/WasherSignUp/WasherSignUpAddress.js
+++ wowo-app/src/components/WasherSignUp/WasherSignUpAddress.js
@@ -9,14 +9,14 @@ export class WasherSignUpAddress extends Component {
     this.autocomplete = null
   }
 
-  // state = {
-  //   phone: this.props.phone,
-  //   street: this.props.street,
-  //   apt: this.props.apt,
-  //   zipCode: this.props.zipCode,
-  //   city: this.props.city,
-  //   usState: this.props.usState
-  // }
+  state = {
+    phone: this.props.phone,
+    street: this.props.street,
+    apt: this.props.apt,
+    zipCode: this.props.zipCode,
+    city: this.props.city,
+    usState: this.props.usState
+  }
   
   componentDidMount() {
     this.autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'), {})
@@ -25,18 +25,20 @@ export class WasherSignUpAddress extends Component {
   }
 
   handlePlaceSelect() {
-    let { values } = this.props;
+    // let { values } = this.props;
+    const { phone, street, apt, zipCode, city, usState } = this.state;
+    const values = { phone, street, apt, zipCode, city, usState };
     let addressObject = this.autocomplete.getPlace();
     let address = addressObject.address_components;
     console.log(address);
-    values = {
-      ...values,
-      street: `${address[0].long_name} ${address[1].long_name}`,
-      apt: address[2].long_name,
-      city: address[3].long_name,
-      usState: address[5].short_name,
-      zipCode: address[7].short_name,
-    }
+    // values = {
+    //   ...values,
+    //   street: `${address[0].long_name} ${address[1].long_name}`,
+    //   apt: address[2].long_name,
+    //   city: address[3].long_name,
+    //   usState: address[5].short_name,
+    //   zipCode: address[7].short_name,
+    // }
     // values.street = `${address[0].long_name} ${address[1].long_name}`;
     // values.apt = address[2].long_name;
     // values.city = address[3].long_name;
@@ -44,19 +46,19 @@ export class WasherSignUpAddress extends Component {
     // values.zipCode = address[7].short_name;
     this.props.handleChange('street');
     console.log('this is the values', values);
-    // this.setState({
-    //   street: `${address[0].long_name} ${address[1].long_name}`,
-    //   apt: address[2].long_name,
-    //   city: address[3].long_name,
-    //   usState: address[5].short_name,
-    //   zipCode: address[7].short_name
-    // })
+    this.setState({
+      street: `${address[0].long_name} ${address[1].long_name}`,
+      apt: address[2].long_name,
+      city: address[3].long_name,
+      usState: address[5].short_name,
+      zipCode: address[7].short_name
+    })
   }
 
   render() {
-    // const { phone, street, apt, zipCode, city, usState } = this.props;
-    // const values = { phone, street, apt, zipCode, city, usState };
-    const { values } = this.props;
+    const { phone, street, apt, zipCode, city, usState } = this.state;
+    const values = { phone, street, apt, zipCode, city, usState };
+    // const { values } = this.props;
     const { handleChange } = this.props;
     const nextBtn = evt => {
       evt.preventDefault();
