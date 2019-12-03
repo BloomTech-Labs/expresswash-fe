47b5c1de821fc5b5beb0336f50058bb31b4f1c39
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-12-03 11:27:56 -0500

    changed ./node_modules/mdbreact to 'mdbreact'

--- wowo-app/src/App.js
+++ wowo-app/src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
-import { Route, Switch } from "react-router-dom";
-import LandingPage from "./components/LandingPage/LandingPage.js";
-import Login from "./components/Login/Login.js";
-import UserSignUp from "./components/UserSignup/UserSignup";
-import WasherSignUp from "./components/WasherSignUp/WasherSignUpForm";
-import "./App.css";
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './components/landingpage/LandingPage.js';
+import Login from './components/login/Login.js';
+import UserSignUp from './components/UserSignup/UserSignup';
+import WasherSignUp from './components/WasherSignUp/WasherSignUpForm';
+import './App.css';
 
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap')
@@ -20,25 +20,25 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const Container = styled.div`
-  height: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: white;
+	height: auto;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background: white;
 `;
 
 function App() {
-  return (
-    <Container className="App">
-      <GlobalStyle />
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/user-register" component={UserSignUp} />
-        <Route path="/login" render={() => <Login />} />
-        <Route path="/washer-register" component={WasherSignUp} />
-      </Switch>
-    </Container>
-  );
+	return (
+		<Container className="App">
+			<GlobalStyle />
+			<Switch>
+				<Route exact path="/" component={LandingPage} />
+				<Route path="/user-register" component={UserSignUp} />
+				<Route path="/login" render={() => <Login />} />
+				<Route path="/washer-register" component={WasherSignUp} />
+			</Switch>
+		</Container>
+	);
 }
 
 export default App;
--- wowo-app/src/components/landingpage/LandingPage.js
+++ wowo-app/src/components/landingpage/LandingPage.js
@@ -1,91 +1,86 @@
-import React from "./node_modules/react";
-import { Link } from "./node_modules/react-router-dom";
-import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBIcon } from "./node_modules/mdbreact";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdbreact';
 
-import "./LandingPage.css";
-import logo from "../../images/wowo-logo-full.JPG";
+import './LandingPage.css';
+import logo from '../../images/wowo-logo-full.JPG';
 
 export default function LandingPage() {
-  return (
-    <MDBContainer className="landing-container">
-      <section className="header-container">
-        <MDBContainer className="landing-header">
-          <MDBRow className="mb-4">
-            <MDBCol>
-              <img className="wowo-logo" src={logo} alt="logo" />
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
+	return (
+		<MDBContainer className="landing-container">
+			<section className="header-container">
+				<MDBContainer className="landing-header">
+					<MDBRow className="mb-4">
+						<MDBCol>
+							<img className="wowo-logo" src={logo} alt="logo" />
+						</MDBCol>
+					</MDBRow>
+				</MDBContainer>
 
-        <MDBContainer className="header-buttons-container">
-          <Link to="user-register">
-            <MDBBtn className="sign-up-btn" color="primary">
-              {" "}
-              Sign Up{" "}
-            </MDBBtn>
-          </Link>
-          <p className="btn-or">or</p>
-          <Link to="login">
-            <MDBBtn className="login-btn"> Log In </MDBBtn>
-          </Link>
-        </MDBContainer>
-      </section>
+				<MDBContainer className="header-buttons-container">
+					<Link to="user-register">
+						<MDBBtn className="sign-up-btn" color="primary">
+							{' '}
+							Sign Up{' '}
+						</MDBBtn>
+					</Link>
+					<p className="btn-or">or</p>
+					<Link to="login">
+						<MDBBtn className="login-btn"> Log In </MDBBtn>
+					</Link>
+				</MDBContainer>
+			</section>
 
-      <section className="landing-information">
-        <MDBContainer className="landing-info-card about">
-          <h3 className="landing-info-header">
-            <MDBIcon icon="car" /> We Bring The Wash To You
-          </h3>
-          <p className="landing-info-content">
-            WoWo is an all new approach to car washing. Instead of driving to
-            the carwash and waiting in long lines, we bring the car wash to you.
-            Just select your wash, type of car, and time and our washers will
-            come to you to perform a quality car wash. Get an instant quote
-            here:
-          </p>
+			<section className="landing-information">
+				<MDBContainer className="landing-info-card about">
+					<h3 className="landing-info-header">
+						<MDBIcon icon="car" /> We Bring The Wash To You
+					</h3>
+					<p className="landing-info-content">
+						WoWo is an all new approach to car washing. Instead of driving to the carwash and waiting in
+						long lines, we bring the car wash to you. Just select your wash, type of car, and time and our
+						washers will come to you to perform a quality car wash. Get an instant quote here:
+					</p>
 
-          {/* <MDBBtn className="quote-btn" color="primary">Instant Quote</MDBBtn> */}
-        </MDBContainer>
+					{/* <MDBBtn className="quote-btn" color="primary">Instant Quote</MDBBtn> */}
+				</MDBContainer>
 
-        <MDBIcon className="info-dot" icon="circle" />
+				<MDBIcon className="info-dot" icon="circle" />
 
-        <MDBContainer className="landing-info-card">
-          <h3 className="landing-info-header eco">
-            <MDBIcon icon="leaf" /> Eco-Friendly
-          </h3>
-          <p className="landing-info-content">
-            WoWo is dedicated to delivering an eco-friendly carwash. All of our
-            washers are trained to used eco-friendly cleaners and practices.
-            That way we can deliver a spectacular car wash while protecting the
-            enviroment.
-          </p>
-        </MDBContainer>
+				<MDBContainer className="landing-info-card">
+					<h3 className="landing-info-header eco">
+						<MDBIcon icon="leaf" /> Eco-Friendly
+					</h3>
+					<p className="landing-info-content">
+						WoWo is dedicated to delivering an eco-friendly carwash. All of our washers are trained to used
+						eco-friendly cleaners and practices. That way we can deliver a spectacular car wash while
+						protecting the enviroment.
+					</p>
+				</MDBContainer>
 
-        <MDBIcon className="info-dot" icon="circle" />
+				<MDBIcon className="info-dot" icon="circle" />
 
+				<MDBContainer className="landing-info-card washers">
+					<h3 className="landing-info-header">
+						<MDBIcon icon="hand-holding-usd" /> Earn Extra Income
+					</h3>
 
-        <MDBContainer className="landing-info-card washers">
-          <h3 className="landing-info-header">
-            <MDBIcon icon="hand-holding-usd" /> Earn Extra Income
-          </h3>
+					<p className="landing-info-content">
+						WoWo welcomes anyone whoe enjoys washing, detailing, or even just looking to make some extra
+						income. All prospective washers must go through training to learn practices and standards. Once
+						the training is complete, the washer can then use the app to find users in need of a carwash.
+						Once the job is completed and the customer satisfied, the washer receives a cut of the cost as
+						well as tips.
+					</p>
+				</MDBContainer>
 
-          <p className="landing-info-content">
-            WoWo welcomes anyone whoe enjoys washing, detailing, or even just
-            looking to make some extra income. All prospective washers must go
-            through training to learn practices and standards. Once the training
-            is complete, the washer can then use the app to find users in need
-            of a carwash. Once the job is completed and the customer satisfied,
-            the washer receives a cut of the cost as well as tips.
-          </p>
-        </MDBContainer>
-
-        <Link to="washer-register">
-          <MDBBtn className="washer-sign-up-btn" color="primary">
-            {" "}
-            Become a Washer{" "}
-          </MDBBtn>
-        </Link>
-      </section>
-    </MDBContainer>
-  );
+				<Link to="washer-register">
+					<MDBBtn className="washer-sign-up-btn" color="primary">
+						{' '}
+						Become a Washer{' '}
+					</MDBBtn>
+				</Link>
+			</section>
+		</MDBContainer>
+	);
 }
