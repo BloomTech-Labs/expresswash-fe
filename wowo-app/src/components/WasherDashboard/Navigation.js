6cd37c0776db3de4c8df4226d94efc6ecfb1b71f
sam-malanchuk <malanchuksam@gmail.com>
2019-12-05 20:59:15 -0500

    added three sample data graphs with react-chartjs-2

--- wowo-app/src/components/WasherDashboard/Navigation.js
+++ wowo-app/src/components/WasherDashboard/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import { MDBCard, MDBContainer, MDBRow, MDBCol, MDBTypography } from "mdbreact";
+import {Line, Doughnut, HorizontalBar} from 'react-chartjs-2';
 
 import Styled from "styled-components";
 import Logo from "../../images/wowo-logo-word-full.svg";
@@ -8,6 +9,69 @@ import Logo from "../../images/wowo-logo-word-full.svg";
 const Img = Styled.img`
     // margin: 25px 0 25px 0;
 `;
+
+const lineData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Monthly Earnings',
+      fill: true,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+
+const donutData = {
+	labels: [
+		'Trucks',
+		'Sedans',
+		'Coupes'
+	],
+	datasets: [{
+		data: [300, 50, 100],
+		backgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		],
+		hoverBackgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		]
+	}]
+};
+
+const horizontalBarData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Miles Driven',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [1300, 1450, 1394, 1634, 1563, 1497, 1395]
+    }
+  ]
+};
 
 export default function Navigation() {
   return (
@@ -79,8 +143,14 @@ export default function Navigation() {
             </MDBCard>
           </MDBCol>
           <MDBCol md="4">
-            <MDBCard>
-              Somethin'
+            <MDBCard className="mb-4">
+              <Line data={lineData} />
+            </MDBCard>
+            <MDBCard className="mb-4">
+              <Doughnut data={donutData} />
+            </MDBCard>
+            <MDBCard className="mb-4">
+              <HorizontalBar data={horizontalBarData} />
             </MDBCard>
           </MDBCol>
         </MDBRow>
