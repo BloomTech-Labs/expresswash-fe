0e96b300a1b02dbd67a2db80fd9d1b35faf67267
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-12-10 14:38:04 -0500

    SingleDate picker working

---
+++ wowo-app/src/components/ClientDashboard/FindWash/ScheduleWash.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css'
+import {SingleDatePicker} from 'react-dates'
+
+
+class DayPicker extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+        startDate:null,
+        endDate:null}
+    }
+    state = {  }
+    render() { 
+        return ( 
+            <div className='daypicker'>
+                <SingleDatePicker
+                orientation="vertical"
+                date={this.state.date} // momentPropTypes.momentObj or null
+                onDateChange={date => this.setState({ date })} // PropTypes.func.isRequired
+                focused={this.state.focused} // PropTypes.bool
+                onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
+                id="your_unique_id" // PropTypes.string.isRequired,
+                />
+            </div>
+
+
+         );
+    }
+}
+ 
+export default DayPicker;
