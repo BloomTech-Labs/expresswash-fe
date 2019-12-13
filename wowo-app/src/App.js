abd7db0f397256e9991a7ee6479b4734c74f4899
MattGThomas <mgthomas0616@gmail.com>
2019-12-12 18:30:16 -0600

    updated to fix some persisting problem

--- wowo-app/src/App.js
+++ wowo-app/src/App.js
@@ -1,17 +1,15 @@
-
-import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
-import { Route, Switch } from 'react-router-dom';
-import LandingPage from './components/landingpage/LandingPage.js';
-import FindWash from './components/ClientDashboard/FindWash/FindWash.js';
-import Login from './components/login/Login.js';
-import UserSignUp from './components/UserSignup/UserSignup';
-import WasherSignUp from './components/WasherSignUp/WasherSignUpForm';
-import ScheduleWash from './components/ClientDashboard/FindWash/ScheduleWash'
-import './App.css';
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { Route, Switch } from "react-router-dom";
+import LandingPage from "./components/landingpage/LandingPage.js";
+import FindWash from "./components/ClientDashboard/FindWash/FindWash.js";
+import Login from "./components/login/Login.js";
+import UserSignUp from "./components/UserSignup/UserSignup";
+import WasherSignUp from "./components/WasherSignUp/WasherSignUpForm";
+import ScheduleWash from "./components/ClientDashboard/FindWash/ScheduleWash";
+import "./App.css";
 
 import Navigation from "./components/ClientDashboard/Navigation";
-
 
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap')
@@ -34,20 +32,20 @@ const Container = styled.div`
 `;
 
 function App() {
-
-	return (
-		<Container className="App">
-			<GlobalStyle />
-			<Switch>
-				<Route exact path="/" component={LandingPage} />
-				<Route path="/user-register" component={UserSignUp} />
-				<Route path="/login" render={() => <Login />} />
-				<Route path="/userDash" component={FindWash} />
-				<Route path="/washer-register" component={WasherSignUp} />
-				<Route path="/schedule" component={ScheduleWash} />
-			</Switch>
-		</Container>
-	);
+  return (
+    <Container className="App">
+      <GlobalStyle />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/user-register" component={UserSignUp} />
+        <Route path="/login" render={() => <Login />} />
+        <Route path="/userDash" component={FindWash} />
+        <Route path="/washer-register" component={WasherSignUp} />
+        <Route path="/schedule" component={ScheduleWash} />
+        <Route path="/user-navigation" component={Navigation} />
+      </Switch>
+    </Container>
+  );
 }
 
 export default App;
--- wowo-app/src/store/configureStore.js
+++ wowo-app/src/store/configureStore.js
@@ -1,15 +1,12 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import rootReducer from '../reducers/rootReducer';
-
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "../reducers/rootReducer";
 
 export default function configureStore() {
   return createStore(
     rootReducer,
-    compose(
-      applyMiddleware(thunk, logger),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 }
