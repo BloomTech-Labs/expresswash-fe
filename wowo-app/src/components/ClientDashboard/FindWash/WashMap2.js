ac7af24c1c2940901faa18d93b6140a25d7759b3
Your Name <melky@Melkys-iMac.local>
2019-12-17 15:35:37 -0500

    add material/picker

---
+++ wowo-app/src/components/ClientDashboard/FindWash/WashMap2.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
+
+function WashMap2() {
+
+  
+  
+  const [viewport, setViewport] = useState([{ width: 100 + '%',height: 100 + '%',
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 8 }])
+
+  return (
+    <ReactMapGL mapboxApiAccessToken= "pk.eyJ1IjoicXVhbjAwNSIsImEiOiJjazN0a2N3a2YwM3ViM2twdzhkbGphMTZzIn0.OepqB_mymhr1YLSYwNmRSg"
+    width={50 + '%'}
+    height={50 + '%'}
+    latitude={37.7577}
+    longitude={-122.4376}
+    zoom={8}
+    onViewportChange={(viewport) => {
+    const {width, height, latitude, longitude, zoom} = viewport;
+    // Optionally call `setState` and use the state to update the map.
+  }}
+/>
+
+  );
+}
+
+export default WashMap2
