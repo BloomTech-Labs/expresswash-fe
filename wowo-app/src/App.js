98cebf442fd06e715f39c484f31203cb6e02c302
MattGThomas <mgthomas0616@gmail.com>
2020-01-08 19:54:54 -0600

    fixed last get request url

--- wowo-app/src/App.js
+++ wowo-app/src/App.js
@@ -1,22 +1,19 @@
-
-import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
-import { Route, Switch } from 'react-router-dom';
-import LandingPage from './components/landingpage/LandingPage.js';
-import FindWash from './components/ClientDashboard/FindWash/FindWash.js';
-import Login from './components/login/Login.js';
-import UserSignUp from './components/UserSignup/UserSignup';
-import WasherNavigation from './components/WasherDashboard/Navigation.js';
-import ClientDashboard from './components/ClientDashboard/FindWash/FindWash.js';
-import WasherSignUp from './components/WasherSignUp/WasherSignUpForm';
-import ScheduleWash from './components/ClientDashboard/FindWash/ScheduleWash';
-import ScheduleWash2 from './components/ClientDashboard/FindWash/ScheduleWash2';
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { Route, Switch } from "react-router-dom";
+import LandingPage from "./components/landingpage/LandingPage.js";
+import FindWash from "./components/ClientDashboard/FindWash/FindWash.js";
+import Login from "./components/login/Login.js";
+import UserSignUp from "./components/UserSignup/UserSignup";
+import WasherNavigation from "./components/WasherDashboard/Navigation.js";
+import ClientDashboard from "./components/ClientDashboard/FindWash/FindWash.js";
+import WasherSignUp from "./components/WasherSignUp/WasherSignUpForm";
+import ScheduleWash from "./components/ClientDashboard/FindWash/ScheduleWash";
+import ScheduleWash2 from "./components/ClientDashboard/FindWash/ScheduleWash2";
+import ClientNavigation from "./components/ClientDashboard/Navigation.js";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
-import './App.css';
-
-import Navigation from './components/ClientDashboard/Navigation';
-
+import "./App.css";
 
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap')
@@ -31,40 +28,37 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const Container = styled.div`
-	height: auto;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	background: white;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: white;
 `;
 
 function App() {
-
-	return (
-		<MuiPickersUtilsProvider utils={DateFnsUtils}>
-		<Container className='App'>
-			<GlobalStyle />
-			<Switch>
-				<Route exact path='/' component={LandingPage} />
-				<Route path='/user-register' component={UserSignUp} />
-				<Route path='/login' render={() => <Login />} />
-				<Route path='/userDash' component={FindWash} />
-				<Route path='/clientDash' component={ClientDashboard} />
-				<Route path='/washer-register' component={WasherSignUp} />
-				<Route path='/washerDash' component={WasherNavigation} />
-				<Route path='/schedule' component={ScheduleWash} />
-				<Route path='/schedule-wash' component={ScheduleWash2}/>
-				
-
-			</Switch>
-			{/* <Route
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Container className="App">
+        <GlobalStyle />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/user-register" component={UserSignUp} />
+          <Route path="/login" render={() => <Login />} />
+          <Route path="/userDash" component={FindWash} />
+          <Route path="/clientDash" component={ClientDashboard} />
+          <Route path="/washer-register" component={WasherSignUp} />
+          <Route path="/washerDash" component={WasherNavigation} />
+          <Route path="/schedule" component={ScheduleWash} />
+          <Route path="/schedule-wash" component={ScheduleWash2} />
+          <Route path="/client-navigation" component={ClientNavigation} />
+        </Switch>
+        {/* <Route
   					path='/schedule-wash'
   					render={(props) => <ScheduleWash2 {...props} isAuthed={true} />}
 /> */}
-		</Container>
-		</MuiPickersUtilsProvider>
-	);
-
+      </Container>
+    </MuiPickersUtilsProvider>
+  );
 }
 
 export default App;
