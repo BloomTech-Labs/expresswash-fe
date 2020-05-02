4efb3835d13621efbd527a754c09cb2c1bac3aaf
DentonWarnock <dentonwarnock@gmail.com>
2020-05-01 20:02:44 -0700

    endpoints changed to local host for testing. register and login client working.

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -26,9 +26,9 @@ export const NEW_CLIENT_SUCCESS = "NEW_CLIENT_SUCCESS";
 export const NEW_CLIENT_ERROR = "NEW_CLIENT_ERROR";
 
 // Add a Car to User signup actions
-export const ADD_CAR_START = 'ADD_CAR_START';
-export const ADD_CAR_SUCCESS = 'ADD_CAR_SUCCESS';
-export const ADD_CAR_FAILED = 'ADD_CAR_FAILED';
+export const ADD_CAR_START = "ADD_CAR_START";
+export const ADD_CAR_SUCCESS = "ADD_CAR_SUCCESS";
+export const ADD_CAR_FAILED = "ADD_CAR_FAILED";
 // Washer Dashboard action types
 export const WASHER_SET_WORK_STATUS_START = "WASHER_SET_WORK_STATUS_START";
 export const WASHER_SET_WORK_STATUS_SUCCESS = "WASHER_SET_WORK_STATUS_SUCCESS";
@@ -44,45 +44,44 @@ export const WASHER_GET_RATING_SUCCESS = "WASHER_GET_RATING_SUCCESS";
 export const WASHER_GET_RATING_FAILED = "WASHER_GET_RATING_FAILED";
 
 export function loginUser(email, password) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch({ type: LOGGING_IN });
 
     return axios
-      .post("https://pt6-wowo.herokuapp.com/authPG/login", { email, password })
-      .then(res => {
+      .post("http://localhost:3300/auth/login", { email, password })
+      .then((res) => {
         console.log(res.data, "res.data");
         localStorage.setItem("token", res.data.token);
-        localStorage.setItem("userType", res.data.userType);
-        localStorage.setItem("id", res.data.id);
-        localStorage.setItem("firstName", res.data.firstName);
-        localStorage.setItem("lastName", res.data.lastName);
-        localStorage.setItem("id", res.data.id);
+        localStorage.setItem("userType", res.data.user.accountType);
+        localStorage.setItem("id", res.data.user.id);
+        localStorage.setItem("firstName", res.data.user.firstName);
+        localStorage.setItem("lastName", res.data.user.lastName);
         console.log(localStorage);
         dispatch({
           type: LOGIN_SUCCESS,
-          payload: res.data
+          payload: res.data,
         });
       })
-      .catch(err => {
+      .catch((err) => {
         dispatch({
           type: LOGIN_FAILED,
-          payload: err
+          payload: err,
         });
       });
   };
 }
 
 export function createClient(payload) {
-  return dispatch => {
+  return (dispatch) => {
     dispatch({ type: LOADING });
 
     return axios
-      .post("https://pt6-wowo.herokuapp.com/authPG/registerClient", payload)
-      .then(res => {
+      .post("http://localhost:3300/auth/registerClient", payload)
+      .then((res) => {
         console.log(res);
         dispatch({ type: NEW_CLIENT_SUCCESS, payload: res.data });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err.response.data);
         dispatch({ type: NEW_CLIENT_ERROR, payload: err.response.data });
       });
@@ -106,45 +105,46 @@ export function createClient(payload) {
 //   }
 // }
 export function getClientInformation(id) {
-  return dispatch => {
+  return (dispatch) => {
     return (
       axios
-        .get(`https://pt6-wowo.herokuapp.com/users/${id}`)
+        .get(`http://localhost:3300/users/${id}`)
         // .get(`http://localhost:3300/users/${id}`)
-        .then(res => {
+        .then((res) => {
           console.log("this is response on getclient information", res);
           dispatch({ type: GET_CLIENT_INFO_SUCCESS, payload: res.data });
         })
-        .catch(err => {
+        .catch((err) => {
           dispatch({ type: GET_CLIENT_INFO_ERROR });
         })
     );
   };
 }
 export function updateClientInformation(id, changes) {
-  return dispatch => {
+  return (dispatch) => {
     return (
       axios
-        .put(`https://pt6-wowo.herokuapp.com/users/${id}`, changes)
+        .put(`http://localhost:3300/users/${id}`, changes)
         // .put(`http://localhost:3300/users/${id}`, changes)
-        .then(res => {
+        .then((res) => {
           dispatch({ type: UPDATE_CLIENT_INFO_SUCCESS, payload: res.data });
         })
-        .catch(err => {
+        .catch((err) => {
           console.log("this is error on update", err);
           dispatch({ type: UPDATE_CLIENT_INFO_ERROR });
         })
     );
   };
 }
+// update to get user by id
 export function getClientCars(id) {
-  return dispatch => {
+  return (dispatch) => {
     return axios
-      .post("https://pt6-wowo.herokuapp.com/carsPG/mycars", id)
-      .then(res => {
+      .get(`http://localhost:3300/users/${id}`)
+      .then((res) => {
         dispatch({ type: GET_CLIENT_CARS_SUCCESS, payload: res.data });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("this is error on get client cars", err);
         dispatch({ type: GET_CLIENT_CARS_ERROR });
       });
@@ -152,14 +152,14 @@ export function getClientCars(id) {
 }
 
 export function getClientRating(id) {
-  return dispatch => {
+  return (dispatch) => {
     return axios
-      .post("https://pt6-wowo.herokuapp.com/ratings/clientaverage", id)
-      .then(res => {
+      .get(`http://localhost:3300/users/${id}`)
+      .then((res) => {
         console.log("this is res on redux call", res);
         dispatch({ type: GET_CLIENT_RATING_SUCCESS, payload: res.data });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("this is error on get client rating", err);
         dispatch({ type: GET_CLIENT_RATING_ERROR });
       });
--- src/actions/washerSignupActions.js
+++ src/actions/washerSignupActions.js
@@ -1,26 +1,27 @@
 // import axios
-import axios from 'axios';
+import axios from "axios";
 
 // import action types
 import {
   CREATE_WASHER_START,
   CREATE_WASHER_SUCCESS,
-  CREATE_WASHER_FAILED
-} from './actionTypes.js';
+  CREATE_WASHER_FAILED,
+} from "./actionTypes.js";
 
 // action creators
 export function register(payload) {
   return (dispatch) => {
-    dispatch({ type: CREATE_WASHER_START })
+    dispatch({ type: CREATE_WASHER_START });
 
-    return axios.post('https://pt6-wowo.herokuapp.com/authPG/registerWasher', payload)
+    return axios
+      .post("http://localhost:3300/auth/registerWasher", payload)
       .then((res) => {
         console.log(res);
-        dispatch({ type: CREATE_WASHER_SUCCESS, payload: res.data })
+        dispatch({ type: CREATE_WASHER_SUCCESS, payload: res.data });
       })
       .catch((err) => {
-        console.log('error message:', err.response.data)
-        dispatch({ type: CREATE_WASHER_FAILED, payload: err.response.data })
-      })
-  }
+        console.log("error message:", err.response.data);
+        dispatch({ type: CREATE_WASHER_FAILED, payload: err.response.data });
+      });
+  };
 }
