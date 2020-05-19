a226d3858f3b0b0571a4e7cd67b61b2840b74590
cladams0203 <cladams0203@gmail.com>
2020-05-19 10:48:46 -0400

    finished find washer test

--- src/components/UserSignup/UserSignup.test.js
+++ src/components/UserSignup/UserSignup.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 import "@testing-library/jest-dom/extend-expect";
 import UserSignup from "./UserSignup";
@@ -8,8 +9,8 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import initialState from "../../reducers/initialState";
 import userReducer from "../../reducers/userReducer";
-import Address from "./signup-steps/Address";
-jest.mock("./signup-steps/Address");
+// import Address from "./signup-steps/Address";
+// jest.mock("./signup-steps/Address");
 
 function renderWithRedux(
   ui,
@@ -20,41 +21,43 @@ function renderWithRedux(
   };
 }
 
-// const setupGoogleMock = () => {
-//   /*** Mock Google Maps JavaScript API ***/
-//   const google = {
-//     maps: {
-//       places: {
-//         Autocomplete: class {},
-//         PlacesServiceStatus: {
-//           INVALID_REQUEST: "INVALID_REQUEST",
-//           NOT_FOUND: "NOT_FOUND",
-//           OK: "OK",
-//           OVER_QUERY_LIMIT: "OVER_QUERY_LIMIT",
-//           REQUEST_DENIED: "REQUEST_DENIED",
-//           UNKNOWN_ERROR: "UNKNOWN_ERROR",
-//           ZERO_RESULTS: "ZERO_RESULTS",
-//         },
-//       },
-//       Geocoder: () => {},
-//       GeocoderStatus: {
-//         ERROR: "ERROR",
-//         INVALID_REQUEST: "INVALID_REQUEST",
-//         OK: "OK",
-//         OVER_QUERY_LIMIT: "OVER_QUERY_LIMIT",
-//         REQUEST_DENIED: "REQUEST_DENIED",
-//         UNKNOWN_ERROR: "UNKNOWN_ERROR",
-//         ZERO_RESULTS: "ZERO_RESULTS",
-//       },
-//     },
-//   };
-//   global.window.google = google;
-// };
+const setupGoogleMock = () => {
+  /*** Mock Google Maps JavaScript API ***/
+  const google = {
+    maps: {
+      places: {
+        Autocomplete: class {
+          addListener() {}
+        },
+        PlacesServiceStatus: {
+          INVALID_REQUEST: "INVALID_REQUEST",
+          NOT_FOUND: "NOT_FOUND",
+          OK: "OK",
+          OVER_QUERY_LIMIT: "OVER_QUERY_LIMIT",
+          REQUEST_DENIED: "REQUEST_DENIED",
+          UNKNOWN_ERROR: "UNKNOWN_ERROR",
+          ZERO_RESULTS: "ZERO_RESULTS",
+        },
+      },
+      Geocoder: () => {},
+      GeocoderStatus: {
+        ERROR: "ERROR",
+        INVALID_REQUEST: "INVALID_REQUEST",
+        OK: "OK",
+        OVER_QUERY_LIMIT: "OVER_QUERY_LIMIT",
+        REQUEST_DENIED: "REQUEST_DENIED",
+        UNKNOWN_ERROR: "UNKNOWN_ERROR",
+        ZERO_RESULTS: "ZERO_RESULTS",
+      },
+    },
+  };
+  global.window.google = google;
+};
 
-// // in test file.
-// beforeAll(() => {
-//   setupGoogleMock();
-// });
+// in test file.
+beforeAll(() => {
+  setupGoogleMock();
+});
 
 test("renders signup page", () => {
   const { getAllByText } = renderWithRedux(
@@ -64,4 +67,32 @@ test("renders signup page", () => {
   );
   const register = getAllByText(/register/i);
   expect(register[0]).toBeInTheDocument();
+});
+
+test("adds input and renders all signup screens", async () => {
+  const { getByLabelText, getByText, getByTestId, debug } = renderWithRedux(
+    <Router>
+      <UserSignup />
+    </Router>
+  );
+  const email = getByLabelText(/email/i);
+  const password = getByLabelText(/password/i);
+  const typeEmail = await userEvent.type(email, "test@test.com");
+  const typePass = await userEvent.type(password, "taco");
+  const register = getByTestId("registerbtn");
+  const registerclick = await userEvent.click(register);
+  expect(getByText(/first name/i)).toBeInTheDocument();
+  expect(getByText(/phone number/i)).toBeInTheDocument();
+  const name = getByLabelText(/first name/i);
+  const lname = getByLabelText(/last name/i);
+  const phnumber = getByLabelText(/phone number/i);
+  await userEvent.type(name, "testfirst");
+  await userEvent.type(lname, "testlast");
+  await userEvent.type(phnumber, "123456789");
+  await userEvent.click(getByText(/next/i));
+  expect(getByText(/state/i)).toBeInTheDocument();
+  await userEvent.click(getByText(/next/i));
+  expect(getByText(/zipcode/i)).toBeInTheDocument();
+  const submit = getByText(/submit/i);
+  expect(submit).toBeInTheDocument();
 });
