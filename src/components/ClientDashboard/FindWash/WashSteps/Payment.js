4071339332ce8aafc56d25feef1f4b285ba4404e
DentonWarnock <dentonwarnock@gmail.com>
2020-05-20 18:54:02 -0700

    forgot to save last commit.

--- src/components/ClientDashboard/FindWash/WashSteps/Payment.js
+++ src/components/ClientDashboard/FindWash/WashSteps/Payment.js
@@ -1,24 +1,14 @@
 import React, { useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
-<<<<<<< HEAD
 import axios from "axios";
-=======
->>>>>>> d1b3b9b2919d81075e6883699478af817cda4853
 
 function Payment() {
   const [product, setProduct] = useState({
     name: "Car Wash",
     price: 40,
-<<<<<<< HEAD
   });
 
   function makePayment(token) {
-=======
-    productBy: "Washer Name",
-  });
-
-  const makePayment = (token) => {
->>>>>>> d1b3b9b2919d81075e6883699478af817cda4853
     const body = {
       token,
       product,
@@ -26,7 +16,6 @@ function Payment() {
     const headers = {
       "Content-Type": "application/json",
     };
-<<<<<<< HEAD
     return axios
       .post(`http://localhost:3300/users/payment`, body)
       .then((res) => {
@@ -34,18 +23,6 @@ function Payment() {
       })
       .catch((err) => console.log(err.message));
   }
-=======
-    return fetch(`http://localhost:3300/users/payment`, {
-      method: "POST",
-      headers,
-      body: JSON.stringify(body),
-    })
-      .then((res) => {
-        console.log("res", res);
-      })
-      .catch((err) => console.log(err));
-  };
->>>>>>> d1b3b9b2919d81075e6883699478af817cda4853
 
   return (
     <div className="payment">
