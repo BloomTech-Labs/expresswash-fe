ba58248ebe808625750578e8e735fce2a15b0957
cladams0203 <cladams0203@gmail.com>
2020-06-06 15:20:32 -0400

    fixed app and washer signup reducer tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,29 @@
 import React from "react";
 // import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+//Redux mock
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers/rootReducer";
+import thunk from "redux-thunk";
+
+function renderWithRedux(
+  ui,
+  {
+    initialState,
+    store = createStore(rootReducer, initialState, applyMiddleware(thunk)),
+  } = {}
+) {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+  };
+}
 
 test("Renders without crashing", async () => {
-  const { getByText } = render(
+  const { getByText } = renderWithRedux(
     <Router>
       <App />
     </Router>
