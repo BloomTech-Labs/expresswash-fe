133039c746917714922434947ed437bc24067e49
DentonWarnock <dentonwarnock@gmail.com>
2020-06-16 20:21:26 -0700

    styling updates for landing page, updated text, fixed washer dashboard jobs bug.

--- src/components/WasherDashboard/JobsDisplay.js
+++ src/components/WasherDashboard/JobsDisplay.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import { DB_URL, getClientInformation } from "../../actions/actionTypes.js";
-import axios from "axios";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { DB_URL, getClientInformation } from '../../actions/actionTypes.js';
+import axios from 'axios';
 
 class JobsDisplay extends Component {
   constructor(props) {
@@ -22,7 +22,7 @@ class JobsDisplay extends Component {
       })
       .then((res) => {
         alert(
-          "Job Successfully Completed! Your payment will show up in your account soon."
+          'Job Successfully Completed! Your payment will show up in your account soon.'
         );
       })
       .catch((err) => console.log(err));
@@ -33,14 +33,15 @@ class JobsDisplay extends Component {
     axios
       .get(`${DB_URL}/jobs/`)
       .then((res) => {
+        console.log('JOBS!', res);
         this.setState({
           jobs: res.data.filter(
-            (job) => job.washerId === localStorage.getItem("washerId")
+            (job) => job.washerId == localStorage.getItem('washerId')
           ),
           completedJobs: res.data.filter(
             (job) =>
-              job.washerId === localStorage.getItem("washerId") &&
-              job.completed === true
+              job.washerId == localStorage.getItem('washerId') &&
+              job.completed == true
           ),
           isGettingJobs: false,
         });
@@ -50,7 +51,7 @@ class JobsDisplay extends Component {
 
   componentWillMount() {
     this.props.getClientInformation(
-      this.props.user.id || localStorage.getItem("id")
+      this.props.user.id || localStorage.getItem('id')
     );
   }
 
@@ -59,11 +60,12 @@ class JobsDisplay extends Component {
   }
 
   render() {
+    console.log('STATE', this.state);
     return (
       <div>
         <div>
           <br />
-          <h2 style={{ fontWeight: "bold" }}>Accepted Jobs</h2>
+          <h2 style={{ fontWeight: 'bold' }}>Accepted Jobs</h2>
           <button
             onClick={() => {
               this.getAvailableJobs();
@@ -73,50 +75,51 @@ class JobsDisplay extends Component {
           </button>
           <hr />
         </div>
-        {this.isGettingJobs === "true" ? (
+        {this.isGettingJobs === 'true' ? (
           <h3>Loading Jobs...</h3>
         ) : (
           <div>
-            {this.state.jobs.map((job) => {
-              return (
-                <div key={job.jobId}>
-                  <h4>Job Address:</h4>
-                  <p>{job.washAddress}</p>
-                  <h4>Job Date and Time:</h4>
-                  <p>{job.timeRequested}</p>
-                  <h4>Payment Before Tip:</h4>
-                  <p>$40.00</p>
+            {this.state.jobs &&
+              this.state.jobs.map((job) => {
+                return (
+                  <div key={job.jobId}>
+                    <h4>Job Address:</h4>
+                    <p>{job.washAddress}</p>
+                    <h4>Job Date and Time:</h4>
+                    <p>{job.timeRequested}</p>
+                    <h4>Payment Before Tip:</h4>
+                    <p>$40.00</p>
 
-                  <button
-                    onClick={() => {
-                      alert("Still need to add this feature!");
-                    }}
-                  >
-                    Upload Before Photo
-                  </button>
-                  <button
-                    onClick={() => {
-                      alert("Still need to add this feature!");
-                    }}
-                  >
-                    Upload After Photo
-                  </button>
-                  <br />
-                  <button
-                    onClick={() => {
-                      this.handleSubmitJob(job.jobId);
-                    }}
-                  >
-                    Mark Job Completed
-                  </button>
-                  <hr />
-                </div>
-              );
-            })}
+                    <button
+                      onClick={() => {
+                        alert('Still need to add this feature!');
+                      }}
+                    >
+                      Upload Before Photo
+                    </button>
+                    <button
+                      onClick={() => {
+                        alert('Still need to add this feature!');
+                      }}
+                    >
+                      Upload After Photo
+                    </button>
+                    <br />
+                    <button
+                      onClick={() => {
+                        this.handleSubmitJob(job.jobId);
+                      }}
+                    >
+                      Mark Job Completed
+                    </button>
+                    <hr />
+                  </div>
+                );
+              })}
           </div>
         )}
         <div>
-          <h2 style={{ fontWeight: "bold" }}>Completed Jobs</h2>
+          <h2 style={{ fontWeight: 'bold' }}>Completed Jobs</h2>
 
           <hr />
         </div>
